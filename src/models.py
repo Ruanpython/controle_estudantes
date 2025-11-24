a69e6df5ce3eada481bb3157074d3b07cc9027de
Ruanpython <rmachadopolonini@gmail.com>
2025-11-24 19:07:50 -0300

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,23 +1,26 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Optional
 
 @dataclass
-class Estudante:
-    id: int | None
-    nome: str
-    matricula: str
-    email: str
-
-@dataclass
-class Curso:
-    id: int | None
-    nome: str
-    carga_horaria: int | None
-
-@dataclass
-class Nota:
-    id: int | None
-    id_estudante: int
+class CursoEmbed: 
     id_curso: int
+    nome_curso: str
+    carga_horaria: int
     nota1: float | None
     nota2: float | None
     media: float | None
+    situacao: str | None = None
+
+@dataclass
+class Estudante:
+    id: Optional[int]
+    nome: str
+    matricula: str
+    email: str
+    cursos: List[CursoEmbed] = field(default_factory=list) 
+@dataclass
+class Curso: 
+    id: int
+    nome: str
+    carga_horaria: int | None
+
